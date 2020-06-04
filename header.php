ed925c66e2ff3d310752ae7f0dceb9815759aa6d
Ivan_Tsarkov <ivtsarkov@bk.ru>
2020-06-04 11:35:12 +0300

    Добавил шапку сайта

---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></head>
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="logo col-md-4">
+        <img src="img/logo.png">
+        <span class="logo-text">Waxom</span>
+      </div>
+      <div class="menu col-md-8">
+        <ul class="nav">
+          <a href="#" class="nav-item">Home</a>
+          <a href="#" class="nav-item">About Us</a>
+          <a href="#" class="nav-item">Portfolio</a>
+          <a href="#" class="nav-item">Features</a>
+          <a href="#" class="nav-item">Blog</a>
+          <a href="#" class="nav-item">Pircing</a>
+          <a href="#" class="nav-item">Shortcodes</a>
+          <a href="#" class="nav-item">Contact</a>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="collumn col-md-6 offset-md-3">
+      <div class="titleone">
+        <span>Unique and Modern Design</span>
+      </div>
+      <div class="titlename">
+        <span>Portfolio PSD Template</span>
+      </div>
+      <div class="titledis">
+        <span>Nam liber tempor cum soluta nobis eleifend option congue nihil imper-<br>diet doming id quod mazim placerat facer possim assum.</span>
+      </div>
+      <div class="button-header">
+        <button type="submit">GET STARTED</button>
+      </div>
+    </div>
+  </div>
+</header>
+</html>
